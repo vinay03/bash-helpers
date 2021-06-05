1e886c16faaa968ab744b857a4637dc060832155
Vinay Jeurkar <vjeurkar-ext@beachbody.com>
2021-06-06 03:02:33 +0530

    Initial Commit

---
+++ helpers.sh
@@ -0,0 +1,27 @@
+source ./prompt.sh
+
+alias nbp="nano ~/.bash_profile"
+alias sbp="source ~/.bash_profile"
+alias cbp="cat ~/.bash_profile"
+
+alias nbr="nano ~/.bashrc"
+alias sbr="source ~/.bashrc"
+alias cbr="cat ~/.bashrc"
+
+alias gst="git status"
+alias gaa="git add ."
+alias gca="git add . && git commit -m $1"
+alias gck="git checkout $1"
+alias gbr="git branch"
+alias gpl='git pull origin $(git rev-parse --abbrev-ref HEAD)'
+alias gps='git push origin $(git rev-parse --abbrev-ref HEAD)'
+
+# Chrome
+alias ojen='open -n -a "Google Chrome" --args "https://jenkins.tools.beachbody.com/"'
+
+
+# Docker
+alias dils="docker image ls"
+alias dps="docker ps"
+alias dpsa="docker ps -a"
+alias dex="docker exec -it $1 /bin/bash"
